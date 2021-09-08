f2f2dfdbe4e6b2d18e974958547c2f88910ffa02
Frolov Vitaly, INI-DOS-O2A (EXT) <Vitaly.Frolov@swisscom.com>
2021-09-08 22:02:45 +0400

    add new pages about and wish_list

--- main/views.py
+++ main/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import WishList
 
 
 def index(request):
     return render(request, 'index.html', {})
+
+
+def about(request):
+    return render(request, 'about.html', {"title": "Wishlist | about project"})
+
+
+def list_page(request, pk):
+    """
+    FBV - views основаны на функциях
+    СBV - views основаны на классах
+    """
+    # print('[PK]', pk)
+    # wishlist = WishList.objects.get(pk=pk)
+    wishlist = get_object_or_404(WishList, pk=pk)
+
+    print([wishlist], wishlist)
+    return render(request,
+                  'wish_list.html',
+                  {'wishlist': wishlist,
+                   'is_owner_list': wishlist.owner == request.user})
